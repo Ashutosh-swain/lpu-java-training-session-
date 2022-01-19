27c60d7e10b1dd62f664bd9a118caf087c1bd9af
Ashutosh-swain <98007133+Ashutosh-swain@users.noreply.github.com>
2022-01-19 11:42:40 +0530

    first program

---
+++ firstprogram.java
@@ -0,0 +1,12 @@
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("our goal is to develop solution to save time:");
+		System.out.println("this is my first program");
+		System.out.println("hellod java world...");
+		System.out.println("i am here to rock and roll..");
+
+	}
+
+}
