02ca2e5cc5a4a48d0d2a22310d8d1e2f82e7b2f3
Ashutosh-swain <98007133+Ashutosh-swain@users.noreply.github.com>
2022-01-20 12:41:22 +0530

    session1 notes

---
+++ session1notes.java
@@ -0,0 +1,13 @@
+
+public class session1notes {
+
+	public static void main(String[] args) {
+		// Simply Code here in the main
+				System.out.println("We are learning java. stick to the concepts well");
+				System.out.println("This is all for the very 1st session");
+				System.out.println("Thank You. I hope you all enjoyed");
+
+
+	}
+
+}
